9cba4f8378b58b7bc3c5cf621c09750b44df196a
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2020-08-13 18:14:30 -0500

    add renderfuntion

--- src/App.js
+++ src/App.js
@@ -4,9 +4,18 @@ import './App.css';
 //importamos reactstrap
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/menuComponent';
+//Importamo9s  the data .would be a better idea to create a api
+import { DISHES } from './shared/dishes';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
   render() {
     return (
       <div className="App">
@@ -15,7 +24,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
